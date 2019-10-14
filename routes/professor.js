f46f18f3cbc1a77ce11461a7ae9e7e51711c551f
thallysbraz <thallysbraz3@gmail.com>
2019-10-14 13:58:55 -0300

    arrumando view de mostrar o professor a sua disciplina

--- routes/professor.js
+++ routes/professor.js
@@ -21,23 +21,20 @@ router.get("/", eProf, (req, res) => {
 });
 router.post("/consulta", async (req, res) => {
   const professor = req.body.matricula;
-  //console.log("professor: ", professor);
-  Usuario.findById({ _id: professor })
-    .then(prof => {
-      if (prof) {
-        res.redirect("/professor/disciplinasList");
-      }
+  const user = req.user._id;
+  console.log("user: ", user);
+  await Disciplina.find({ professor })
+    .then(disciplinas => {
+      res.render("professor/disciplinas", { disciplinas: disciplinas });
     })
     .catch(err => {
-      req.flash("error_msg", "Professor não encontrado, tente novamente");
-      res.redirect("/professor");
+      req.flash(
+        "error_msg",
+        "Houve error ao listar as discplinas que vc da aula"
+      );
+      req.redirect("/professor");
     });
-});
-//rota para mostrar ao professor suas disciplinas
-router.get("/disciplinasList", async (req, res) => {
-  const professor = res.locals.user._id;
-  console.log("professor: ", professor);
-  res.send(professor);
+
   /*try {
     const disciplina = await Disciplina.find({ professor });
     return res.send({ disciplina });
@@ -45,6 +42,21 @@ router.get("/disciplinasList", async (req, res) => {
     console.log(err);
     return res.status(400).send({ error: "Error, loading disciplina" });
   }*/
+});
+//rota para mostrar ao professor suas disciplinas
+router.get("/disciplinasList", async (req, res) => {
+  const professor = res.locals.user._id;
+  console.log("professor: ", professor);
+  //res.send(professor);
+  try {
+    const disciplina = await Disciplina.find({ professor }).populate([
+      "matriculados"
+    ]);
+    return res.send({ disciplina });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send({ error: "Error, loading disciplina" });
+  }
 });
 
 router.get("/disciplinas", eProf, (req, res) => {
